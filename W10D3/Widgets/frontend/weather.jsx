77b619c4fc1dcb37e3fdf4e0f387b48f5b12d67f
Michelle Lin <michelleamazinglin@gmail.com>
2020-11-04 17:34:49 -0500

    finish working

--- W10D3/Widgets/frontend/weather.jsx
+++ W10D3/Widgets/frontend/weather.jsx
@@ -10,45 +10,37 @@ export default class Weather extends React.Component {
 
     componentDidMount() {
         let results;
-        navigator.geolocation.getCurrentPosition(position => results = position);
+        navigator.geolocation.getCurrentPosition(this.success);
+    }
+    
+    success(position) {
+        // debugger;
         const apiKey = "3ca7b42669aa7a1cc5cf8243013051c1";
         const params = {
-            latitude: results.coords.latitude,
-            longitude: results.coords.longitude
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
         };
         const url = `http://api.openweathermap.org/data/2.5/weather?lat=${params.latitude}&lon=${params.longitude}&appid=${apiKey}`;
         const xmlhttp = new XMLHttpRequest();
+        // console.log(xmlhttp.responseURL);
+        // xmlhttp.onload = () => {
+        //     console.log(xmlhttp.responseURL);
+        // }
+        xmlhttp.onreadystatechange = function () {
+            debugger;
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+                if (xmlhttp.status == 200) {
+                    // document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+                    console.log(`The response is: ${xmlhttp.responseText}`);
+                } else if (xmlhttp.status == 400) {
+                    alert('There was an error 400');
+                } else {
+                    alert('something else other than 200 was returned');
+                }
+        }
         xmlhttp.open("GET", url);
-        xmlhttp.onload = () => {
-            console.log(xmlhttp.responseURL);
-        }
-
+      }
     }
-
-    /*
-    <script type="text/javascript">
-function loadXMLDoc() {
-    var xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
-           if (xmlhttp.status == 200) {
-               document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
-           }
-           else if (xmlhttp.status == 400) {
-              alert('There was an error 400');
-           }
-           else {
-               alert('something else other than 200 was returned');
-           }
-        }
-    };
-
-    xmlhttp.open("GET", "ajax_info.txt", true);
-    xmlhttp.send();
-}
-</script>
-    */
 
     render() {
         return (
